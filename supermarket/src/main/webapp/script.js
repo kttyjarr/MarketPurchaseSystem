979359873754456ea8a894224ddc242ce0d1b356
Kurt <kurtrojo83@gmail.com>
2024-11-21 01:03:54 +0800

    added mainpage

--- supermarket/src/main/webapp/script.js
+++ supermarket/src/main/webapp/script.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('registration-form').addEventListener('submit', function(event) {
-        var password = document.querySelector('input[name="password"]').value;
-        var confirmPassword = document.querySelector('input[name="confirmPassword"]').value;
-        if (password !== confirmPassword) {
-            event.preventDefault();
-            document.getElementById('error-message').style.display = 'block';
-        }
-    });
-});
+
